494e67c904d1b55375ba1232a2c394349460c2dd
GitHub Action <action@github.com>
2022-09-10 07:47:03 +0000

    Template cleanup

--- src/main/kotlin/com/github/arslanmt13js/translate/MyBundle.kt
+++ src/main/kotlin/com/github/arslanmt13js/translate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.arslanmt13js.translate
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/arslanmt13js/translate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/arslanmt13js/translate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.arslanmt13js.translate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.arslanmt13js.translate.MyBundle
 
 class MyApplicationService {
 
